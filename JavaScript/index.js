e8e415bf3c3bc818a19298449c9f796b7d9d53fb
koppunoori shashank <36255502+shashank298@users.noreply.github.com>
2020-12-30 03:38:46 +0530

    Add files via upload

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,12 +1,8 @@
-// const oval = document.querySelector('.oval')
-// const diamond = document.querySelector(".diamond");
-// const rectangle = document.querySelector('.rectangle')
-// const parallelogram = document.querySelector(".parallelogram");
-// const arrow = document.querySelector('.arrow')
-const items=document.querySelectorAll('.item')
+
+const items = document.querySelectorAll('.item')
 const empty = document.querySelector('.empty')
-const container = document.querySelector("#container");
- let nodeList=[]
+let nodeList = []
+ 
  let active = false;
  let currentX;
  let currentY;
@@ -14,14 +10,21 @@ const container = document.querySelector("#container");
  let initialY;
  let xOffset = 0;
  let yOffset = 0;
-let activeNode; 
-// fill.addEventListener("dragstart", dragstart);
-// fill.addEventListener("dragend", dragend);
-container.addEventListener("touchstart", moveStart, false);
-container.addEventListener("touchend", moveEnd, false);
-container.addEventListener("touchmove", move, false);
+let activeItem; 
 
-container.addEventListener("mousedown", moveStart, false);
+ const container = document.querySelector("#container");
+ const containerLeftX = container.getBoundingClientRect().left;
+ const containerRightX = container.getBoundingClientRect().right;
+ const containerBottomY = container.getBoundingClientRect().bottom;
+const containerTopY = container.getBoundingClientRect().top;
+ 
+const deleteBlock = document.querySelector('#delete')
+const deleteBlockwidth = deleteBlock.getBoundingClientRect().right;
+const deleteBlockHeight = deleteBlock.getBoundingClientRect().top;
+const deleteBlockBottom= deleteBlock.getBoundingClientRect().bottom;
+
+
+container.addEventListener("mousedown", moveStart);
 container.addEventListener("mouseup", moveEnd, false);
 container.addEventListener("mousemove", move, false);
 
@@ -29,7 +32,7 @@ container.addEventListener("mousemove", move, false);
 
 for (const item of items) {
     item.addEventListener("dragstart", dragstart);
-    item.addEventListener("dragend", dragend);
+  item.addEventListener("dragend", dragend);
 }
 
     empty.addEventListener("dragover", dragOver);
@@ -39,53 +42,90 @@ for (const item of items) {
 
 
 
+
 function dragstart(e) {
   e.dataTransfer.setData('text/html',e.target.id)
-    // this.className += " hold"
-    // setTimeout(()=>this.className="invisible",0)
-  
-    
 }
 
 function moveStart(e) {
-  nodeList = document.querySelectorAll("#container .item");
-  console.log(e)
- for (let node of nodeList) {
-     if (node.isEqualNode(e.target)) {
-       active = true;
-       activeNode = node;
-       initialX = e.offsetX; 
-       initialY = e.offsetY; 
-    }
-  }
+ 
+ if (e.target !== e.currentTarget) {
+   active = true;
+   activeItem = e.target;
+
+   if (activeItem !== null) {
+     if (!activeItem.xOffset) {
+       activeItem.xOffset = 0;
+     }
+
+     if (!activeItem.yOffset) {
+       activeItem.yOffset = 0;
+     }
+
+     activeItem.initialX = e.clientX - activeItem.xOffset;
+     activeItem.initialY = e.clientY - activeItem.yOffset;
+   }
+
+  
+    
+ }
   
 }
 
 function moveEnd(e) {
-  initialX = currentX;
-  initialY = currentY;
+  if (activeItem !== null) {
+    activeItem.initialX = activeItem.currentX;
+    activeItem.initialY = activeItem.currentY;
+  }
 
   active = false;
+  activeItem = null;
 }
 
 function move(e) {
-  if (active) {
+  if (active ) {
+    console.log("element type", e.target.nodeName === "DIV");
     e.preventDefault();
 
-    if (e.type === "touchmove") {
-      currentX = e.touches[0].clientX - initialX;
-      currentY = e.touches[0].clientY - initialY;
-    } else {
-      currentX = e.clientX - initialX;
-      currentY = e.clientY - initialY;
+    // console.log("clientY", e.clientY);
+    //      console.log("pagex",e.pageX)
+    //console.log("offset", e.offsetY)
+    //   console.log("screenx", e.screenX)
+
+    const height = e.target.getBoundingClientRect().height;
+    const width = e.target.getBoundingClientRect().width;
+    const currentItemLeftPosition = e.clientX - e.offsetX;
+    const currentItemRightPosition = e.clientX - e.offsetX + width;
+    const currentItemTopPosition = e.clientY - e.offsetY;
+    const currentItemBottomPosition = e.clientY - e.offsetY + height;
+    console.log("left",currentItemLeftPosition)
+    console.log("comp left",deleteBlockwidth)
+    console.log("ITM BTM", currentItemBottomPosition);
+    console.log("del btm", deleteBlockHeight)
+    if (currentItemLeftPosition < deleteBlockwidth-4 && currentItemBottomPosition > deleteBlockHeight) {
+      const deleteNodeId =e.target.id;
+      document.getElementById(deleteNodeId).remove()
     }
+    // if (
+    //   currentItemRightPosition > containerRightX ||
+    //   currentItemLeftPosition < containerLeftX ||
+    //   currentItemTopPosition < containerTopY ||
+    //   currentItemBottomPosition > containerBottomY
+    // ) {
+    //   console.log("Out of range");
+    // } else {
+    
+      activeItem.currentX = e.clientX - activeItem.initialX;
+    activeItem.currentY = e.clientY - activeItem.initialY;
 
-    xOffset = currentX;
-    yOffset = currentY;
-    setTranslate(currentX, currentY, activeNode);
+    activeItem.xOffset = activeItem.currentX;
+    activeItem.yOffset = activeItem.currentY;
 
+    setTranslate(activeItem.currentX, activeItem.currentY, activeItem);
+  
   }
 }
+
 
 function setTranslate(xPos, yPos, el) {
   el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
@@ -109,14 +149,61 @@ function dragend() {
     // this.className = "empty";
   
 }
+
+
 function dragDrop(e) {
   e.preventDefault()
   const data = e.dataTransfer.getData("text/html");
+  const xOffset=e.offsetX
+  const yOffset=e.offsetY
   const nodeCopy = document.getElementById(data).cloneNode(true)
-  nodeCopy.id = `${nodeCopy.id}${Math.floor(Math.random()*10000)}`;
-  e.target.appendChild(nodeCopy)
-   
-    // this.className = "empty";
-    // this.append(fill)
+ nodeCopy.style.width = "48px";
+  
+  const id = `${nodeCopy.id}${Math.floor(Math.random() * 10000)} resizable`;
+  nodeCopy.id = id
+  nodeCopy.className += " resizable"
+  let classs = nodeCopy.className.split(" ")[1]
 
+  if (classs === "line") {
+    nodeCopy.className += " rotate ";
+  }
+
+  if (classs === 'rectangle') {
+     nodeCopy.style.height = "55px";
+     nodeCopy.style.width = "80px";
+  }
+  if (classs === "parallelogram") {
+    nodeCopy.style.height = "50px";
+    nodeCopy.style.width = "80px";
+  }
+  if (classs === "oval") {
+     nodeCopy.style.height = "40px";
+     nodeCopy.style.width = "80px";
+  }
+  if (classs !=="line") {
+    const inputElement = document.getElementById("input").cloneNode(true);
+    inputElement.style.resize = "both";
+    nodeCopy.appendChild(inputElement);
+
+  }
+  e.target.appendChild(nodeCopy);
+  console.log(nodeCopy.className)
+  rotateArrow()
+}
+
+function rotateArrow() {
+  const arrows = document.querySelectorAll(".rotate");
+  console.log(arrows)
+  for (const arrow of arrows) {
+    arrow.addEventListener("dblclick", function () {
+      
+      const div = document.getElementById(this.id);
+      let rStyle = div.style.webkitTransform;
+      let deg = 90;
+      if (rStyle) {
+        deg = Number(rStyle.replace(/[^\d.]/g, "")) + 90;
+      }
+      
+    });
+  }
 }
